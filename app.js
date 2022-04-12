04d18ddb868d04dd865bae30fa22175913493e12
ajay-wh1 <ajaya@warehousing1.com>
2022-04-12 11:32:53 +0545

    change message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("Hello Youtube! boom boom!"));
 
 app.get("/test", (req, res) => res.send("Hello Youtube 123!"));
 
